5b42784c17a2e7aa8e6563fb33a5669b2371f1a3
archieLa <arturm.lach@gmail.com>
2019-01-11 23:13:17 -0600

    First implementation of doors switches driver

---
+++ DoorsSwitchesDriver.cpp
@@ -0,0 +1,75 @@
+#include "DoorsSwitchesDriver.h"
+
+
+const uint8_t DoorsSwitchesDriver::mMCP1_PIN_TO_DOOR[15] = {17, 3, 23, 8, 10, 20, 1, 22, 21, 7, 5, 9, 
+                                                            24, 13, 19};
+const uint8_t DoorsSwitchesDriver::mMCP2_PIN_TO_DOOR[13] = {255, 255, 255, 255, 6, 12, 11, 18, 15, 4,
+                                                            16, 2, 14};
+
+DoorsSwitchesDriver* DoorsSwitchesDriver::mItself = NULL;
+
+void DoorsSwitchesDriver::handleDoorSwInterrupt()
+{
+    debugLogger.log("MCP1_Interrupt\n");
+    if (mItself)
+    {
+    mItself->handle_interrupt(1);
+    }
+    else
+    {
+        debugLogger.log("Callback not set up\n");
+    }
+}
+
+void DoorsSwitchesDriver::handleDoorSwInterrupt2()
+{
+    debugLogger.log("MCP2_Interrupt\n");
+    if (mItself)
+    {
+        mItself->handle_interrupt(2);
+    }
+    else
+    {
+        debugLogger.log("Callback not set up\n");
+    }
+}
+
+
+void DoorsSwitchesDriver::handle_interrupt(uint8_t mcpNumber)
+{
+    
+    uint8_t pin;
+
+    if (mcpNumber == 1)
+    {
+        pin = mcp.getLastInterruptPin();
+        mLastDoorNumberTriggered = mMCP1_PIN_TO_DOOR[pin];
+        while (pin != MCP23017_INT_ERR) 
+        {
+            mcp.digitalRead(pin);
+            pin = mcp.getLastInterruptPin();
+        }
+    }
+    else
+    {
+        pin = mcp2.getLastInterruptPin();
+        mLastDoorNumberTriggered = mMCP2_PIN_TO_DOOR[pin];
+        while (pin != MCP23017_INT_ERR) 
+        {
+            mcp2.digitalRead(pin);
+            pin = mcp2.getLastInterruptPin();
+        }
+    }
+
+    debugLogger.log(mDEBUGSTR2, __FUNCTION__, mLastDoorNumberTriggered);
+    
+    if (mSubscriber == NULL)
+    {
+        debugLogger.log("No callback object");           
+    }
+    else
+    {
+        mSubscriber->handle_callback(Common::DOOR_OPENED, reinterpret_cast<void*>(mLastDoorNumberTriggered));
+    }
+
+}
---
+++ DoorsSwitchesDriver.h
@@ -0,0 +1,118 @@
+#ifndef _DOORSSWITCHESDRIVER_H_
+#define _DOORSSWITCHESDRIVER_H_
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_MCP23017.h>
+#include "Common.h"
+
+class DoorsSwitchesDriver 
+{
+    public:
+    DoorsSwitchesDriver(uint8_t addr1 = 0, uint8_t addr2 = 1, uint8_t intPin1 = 30, uint8_t intPin2 = 31) :
+    mADDR1(addr1), mADDR2(addr2), mINTPIN1(intPin1), mINTPIN2(intPin2), mInterrStopped(true)
+    {
+        debugLogger.log(mDEBUGSTR1, __FUNCTION__);
+        // Initialize pin on the microcontroller
+        pinMode(mINTPIN1, INPUT);
+        pinMode(mINTPIN2, INPUT);
+        // Set ups interr on mcp chips
+        mcp.setupInterrupts(true,false,LOW);
+        mcp2.setupInterrupts(true,false,LOW);
+        // Initializes pin on mcp chips
+        // Set up all the pins on the first mcp
+        for (int i = 0; i < 15; i++)
+        {
+            mcp.pinMode(i, INPUT);
+            mcp.pullUp(i, HIGH);  // turn on a 100K pullup internally
+            mcp.setupInterruptPin(i, RISING);     
+        }
+        // Set up pins used on the second mcp
+        for (int i = 4; i < 8; i++)
+        {
+            mcp2.pinMode(i, INPUT);
+            mcp2.pullUp(i, HIGH);  // turn on a 100K pullup internally
+            mcp2.setupInterruptPin(i, RISING); 
+        }
+
+        for (int i = 8; i < 12; i++)
+        {
+            mcp2.pinMode(i, INPUT);
+            mcp2.pullUp(i, HIGH);  // turn on a 100K pullup internally
+            mcp2.setupInterruptPin(i, RISING); 
+        }
+
+        // Due to hardware issue I had one switch is not connected to first mcp
+        // but added to the second on pin 12
+        mcp2.pinMode(12, INPUT);
+        mcp2.pullUp(12, HIGH);
+        mcp2.setupInterruptPin(12, RISING);
+
+        // attach interrupt functions
+        attachInterrupt(digitalPinToInterrupt(mINTPIN1), DoorsSwitchesDriver::handleDoorSwInterrupt, FALLING);
+        attachInterrupt(digitalPinToInterrupt(mINTPIN2), DoorsSwitchesDriver::handleDoorSwInterrupt2, FALLING);
+        mInterrStopped = false;
+    }
+    
+    void begin()
+    {
+        debugLogger.log(mDEBUGSTR1, __FUNCTION__);
+        mcp.begin(mADDR1);
+        mcp2.begin(mADDR2);
+    }
+
+    void stop_interrupts()
+    {
+        debugLogger.log(mDEBUGSTR1, __FUNCTION__);
+        if (!mInterrStopped)
+        {
+            detachInterrupt(digitalPinToInterrupt(mINTPIN1));
+            detachInterrupt(digitalPinToInterrupt(mINTPIN2));
+            mInterrStopped = true;
+        }
+    }
+
+    void start_interrupts()
+    {
+        debugLogger.log(mDEBUGSTR1, __FUNCTION__);
+        if (mInterrStopped)
+        {
+            attachInterrupt(digitalPinToInterrupt(mINTPIN1), handleDoorSwInterrupt, FALLING);
+            attachInterrupt(digitalPinToInterrupt(mINTPIN2), handleDoorSwInterrupt2, FALLING); 
+            mInterrStopped = false;  
+        }
+    }
+
+    void subscribe_to_sw_event(Subscriber* subscriber)
+    {
+        debugLogger.log(mDEBUGSTR1, __FUNCTION__);
+        mSubscriber = subscriber;
+        mItself = this;
+    }
+
+    void handle_interrupt(uint8_t mcpNumber);
+
+    static void handleDoorSwInterrupt();
+    static void handleDoorSwInterrupt2();
+    
+    
+    private:
+    const char* mDEBUGSTR1 = "Door SW Driver: %s\n";
+    const char* mDEBUGSTR2 = "Door SW Driver: %s, door #: %d\n";     
+    static const uint8_t mMCP1_PIN_TO_DOOR[15];
+    static const uint8_t mMCP2_PIN_TO_DOOR[13];
+    const uint8_t mADDR1;
+    const uint8_t mADDR2;
+    const uint8_t mINTPIN1;
+    const uint8_t mINTPIN2;
+    bool mInterrStopped;
+    Subscriber* mSubscriber;
+    uint8_t mLastDoorNumberTriggered;
+    Adafruit_MCP23017 mcp;
+    Adafruit_MCP23017 mcp2;
+    static DoorsSwitchesDriver* mItself;
+    
+
+};
+
+
+#endif
